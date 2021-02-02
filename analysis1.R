9743f8cb23dc944aba6d097f8f6efee83b32ec74
Weinberger <daniel.weinberger@yale.edu>
2021-02-02 23:19:24 +0100

    update; filter low SD ZIPs that look like they result from convergence issues

--- analysis1.R
+++ analysis1.R
@@ -2,36 +2,44 @@ library(rstan)
 library(HDInterval)
 options(mc.cores = (parallel::detectCores()-1))
 rstan_options(auto_write = TRUE)
-# 
-#  d1 <- as.matrix(readRDS('./Data/firstny.rds'))
-#  d2 <- as.matrix(readRDS('./Data/nyamp.rds'))
-# 
-#  phase.med <- apply(d1,2, median)
-#  phase.sd <- apply(d1,2, sd)
-# 
-#  amp.med <- apply(d2,2,median)
-#  amp.sd <- apply(d2,2,sd)
-#  amp.med[amp.med>15] <- 15 #pulls outliers back in
-#  amp.sd[amp.sd>100] <- 100 #pulls outliers back in
-#  
-#  amp.prec <- 1/amp.sd^2
-#  amp.inv.var <- 1/amp.sd^2
-#  wgt.mean.amp <- weighted.mean(amp.med, 1/amp.sd^2)
-#  wgt.var <- sum(amp.inv.var * (amp.med - wgt.mean.amp)^2) #weighted variance
-# 
-#  amp.med.std <- (amp.med - wgt.mean.amp)/sqrt(wgt.var) #standardize amplitude, using weighted values
-#  amp.sd.std <- amp.sd/sqrt(wgt.var) #scale sd
-# 
-# 
-#  input.data.list <- list('phase.med'=phase.med,'phase.sd'=phase.sd,'amp.med.std'=amp.med.std, 'amp.sd.std'=amp.sd.std)
-#  saveRDS(input.data.list,'./Data/input_data.rds')
-# 
- input.data.list <- readRDS('./Data/input_data.rds')
+
+ # d1 <- as.matrix(readRDS('./Data/firstny.rds'))
+ # d2 <- -1*pi/6*as.matrix(readRDS('./Data/nyamp.rds')) #multiplyin by -pi/6 converts from scale of amplitude to max derivative
+ # #d2 <- as.matrix(readRDS('./Data/nyamp.rds')) #multiplyin by -pi/6 converts from scale of amplitude to max derivative
+ # 
+ # phase.med <- apply(d1,2, median)
+ # phase.sd <- apply(d1,2, sd)
+ # 
+ # amp.med <- apply(d2,2,median)
+ # amp.sd <- apply(d2,2,sd)
+ # amp.med[amp.med < -5 ] <- -5 #pulls outliers back in
+ # amp.sd[amp.sd>5] <- 5 #pulls outliers back in
+ # 
+ # amp.prec <- 1/amp.sd^2
+ # amp.inv.var <- 1/amp.sd^2
+ # wgt.mean.amp <- weighted.mean(amp.med, 1/amp.sd^2)
+ # wgt.var <- sum(amp.inv.var * (amp.med - wgt.mean.amp)^2) #weighted variance
+ # 
+ # amp.med.std <- (amp.med - wgt.mean.amp)/sqrt(wgt.var) #standardize amplitude, using weighted values
+ # amp.sd.std <- amp.sd/sqrt(wgt.var) #scale sd
+ # 
+ # 
+ # input.data.list <- list('phase.med'=phase.med,'phase.sd'=phase.sd,'amp.med.std'=amp.med.std, 'amp.sd.std'=amp.sd.std)
+ # saveRDS(input.data.list,'./Data/input_data.rds')
+
+input.data.list <- readRDS('./Data/input_data.rds')
 
 phase.med <- input.data.list$phase.med
 phase.sd <- input.data.list$phase.sd
 amp.med.std <- input.data.list$amp.med.std
 amp.sd.std <- input.data.list$amp.sd.std
+
+exclude <- which(amp.med.std > 0.0008 )
+
+phase.med <- phase.med[-exclude]
+phase.sd <- phase.sd[-exclude]
+amp.med.std <- amp.med.std[-exclude]
+amp.sd.std <- amp.sd.std[-exclude]
 
 plot( amp.med.std,phase.med)
 
@@ -72,7 +80,7 @@ symbols(x.hdi$x.med, true.y.med, sqrt((1/phase.sd^2)/pi),fg=NA, bg=rgb(0,0,1,0.0
 abline(a=alpha.med, b=beta.med, col='red')
 
 #Observed data with regression line overlaid
-symbols(amp.med.std, phase.med, sqrt((1/phase.sd^2)/pi),fg=NA, bg=rgb(0,0,1,0.05) , inches=0.15, ylim=c(-1,1), xlim=c(0,0.15) )
+symbols(amp.med.std, phase.med, sqrt((1/phase.sd^2)/pi),fg=NA, bg=rgb(0,0,1,0.05) , inches=0.15 )
 abline(a=alpha.med, b=beta.med, col='red')
 
 
